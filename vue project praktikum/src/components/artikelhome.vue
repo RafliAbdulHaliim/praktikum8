5941d628859e78cac33eab6e22e36aa85072dc8f
rafliabdulhalim <rafliabdulhalm@students.amikom.ac.id>
2024-05-30 17:15:32 +0700

    praktikum8

---
+++ vue project praktikum/src/components/artikelhome.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>ini adalah komponen artikel</h1>
+        <p>ini adalah paragraf</p>
+    </div>
+</template>
---
+++ vue project praktikum/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import artikelhome from '../components/artikelhome.vue'
+</script>
+
+<template>
+  <main>
+    <artikelhome />
+  </main>
+</template>
